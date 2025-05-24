c38dd360e3a68c019eea9bf1ac38ce13888e8101
Dawid7212 <dawidMoeck002@gmail.com>
2025-05-24 10:25:24 +0200

    normalizowanie probek

--- Program.cs
+++ Program.cs
@@ -25,7 +25,38 @@ namespace K_NNalgorytm
             double[] min = new double[Probki[0].Length-1]; //są trzy kolumny wiec trzy minima
             double[] max = new double[Probki[0].Length - 1];
 
-            
+            for (int i = 0; i < min.Length; i++)
+            {
+                min[i] = Probki[0][i];
+                max[i] = Probki[0][i];
+                for (int s = 1; s < Probki.Length; s++)
+                {
+                    if (Probki[s][i] < min[i])
+                    {
+                        min[i] = Probki[s][i];
+                    }
+                    if (Probki[s][i] > max[i])
+                    {
+                        max[i] = Probki[s][i];
+                    }
+                }
+            }
+            for (int s = 0; s < Probki.Length; s++)
+            {
+                Znormalizowane[s] = new double[Probki[s].Length];
+                for (int j = 0; j < Probki[0].Length - 1; j++)
+                {
+                    if (max[j] == min[j])//  jak wartości sa tekie same to 0.0
+                    {
+                        Znormalizowane[s][j] = 0.0;
+                    }
+                    else
+                    {
+                        Znormalizowane[s][j] = (Probki[s][j] - min[j]) / (max[j] - min[j]);
+                    }    
+                }
+                Znormalizowane[s][Probki[0].Length - 1] = Probki[s][Probki[0].Length - 1]; //ostania kolumna dla klasy
+            }
             return Znormalizowane;
         }
 
@@ -42,8 +73,22 @@ namespace K_NNalgorytm
                 }
                 Console.WriteLine();
             }
+
+            Console.WriteLine();
+
+
+            double[][] znormalizowane = Normalizacja(BazaProbek);
+            foreach (double[] element in znormalizowane)
+            {
+                foreach (double value in element)
+                {
+                    Console.Write(value + "  ");
+                }
+                Console.WriteLine();
+            }
             Console.ReadKey();
         }
+        
         public static double[][] BazaProbek = new double[][]
         {           
         };
